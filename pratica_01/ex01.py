114ee442d5a4bd9f788382c51e39d45563e2d56c
kathy <kathelyn.dfsales@gmail.com>
2026-04-19 11:23:26 -0300

    feat: entrega aula 01 - Kathelyn Fontoura

---
+++ pratica_01/ex01.py
@@ -0,0 +1,5 @@
+nome = input("Digite o seu nome: ")
+ano_nascimento = int(input("Digite o seu ano de nascimento: "))
+altura = float(input("Digite a sua altura: "))
+
+print(f"Olá, {nome}. Você tem {2026 - ano_nascimento} anos e mede {altura} metros de altura.")
